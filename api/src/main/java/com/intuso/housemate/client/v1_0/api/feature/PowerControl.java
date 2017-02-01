7c29f8fc2080a31fe495f5a7fc29ab3949fd067d
Tom Clabon <tom@intuso.com>
2017-02-01 13:07:26 +0000

    Updated api interfaces (eg power, playback) so they are stateless. We can make proxy versions that have state. Listeners are (and should be from now on) called as they are added

--- api/src/main/java/com/intuso/housemate/client/v1_0/api/feature/PowerControl.java
+++ api/src/main/java/com/intuso/housemate/client/v1_0/api/feature/PowerControl.java
@@ -26,36 +26,19 @@ public interface PowerControl {
     void turnOff();
 
     /**
-     * API for controlling power with state
+     * Add a listener
      */
-    @Id(value = "power-stateful", name = "Power", description = "Power")
-    interface Stateful extends PowerControl {
-
-        String ID = Stateful.class.getAnnotation(Id.class).value();
-
-        /**
-         * Get whether the device is current on
-         * @return true if the device is currently on
-         */
-        @Value
-        @Id(value = "on", name = "On", description = "True if the device is currently on")
-        boolean isOn();
-
-        /**
-         * Add a listener
-         */
-        @AddListener
-        ManagedCollection.Registration addListener(Listener listener);
-    }
+    @AddListener
+    ManagedCollection.Registration addListener(Listener listener);
 
     interface Listener {
 
         /**
          * Callback for when the device has been turned on or off
-         * @param on true if the device is now on
+         * @param on true if the device is now on, null if unknown
          */
         @Value
         @Id(value = "on", name = "On", description = "True if the device is now on")
-        void on(boolean on);
+        void on(Boolean on);
     }
 }
